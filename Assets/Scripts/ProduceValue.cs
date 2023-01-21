e78aecfa79d2761f7bde9facdb662ed7c2340c52
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2023-01-21 09:17:29 +0300

    ProduceValue with default values (0, 0)

--- Assets/Scripts/ProduceValue.cs
+++ Assets/Scripts/ProduceValue.cs
@@ -3,13 +3,17 @@ public struct ProduceValue
 {
     public int gold;
     public int actions;
-    
-    public static ProduceValue operator +(ProduceValue a, ProduceValue b) => new ProduceValue(a.gold + b.gold, a.actions + b.actions);
+
+    public static ProduceValue operator +(ProduceValue a, ProduceValue b) =>
+        new ProduceValue(a.gold + b.gold, a.actions + b.actions);
+
     public static ProduceValue operator +(ProduceValue a) => a;
     public static ProduceValue operator -(ProduceValue a) => new ProduceValue(-a.gold, -a.actions);
-    public static ProduceValue operator -(ProduceValue a, ProduceValue b) => new ProduceValue(a.gold - b.gold, a.actions - b.actions);
-    
-    public ProduceValue(int gold, int actions)
+
+    public static ProduceValue operator -(ProduceValue a, ProduceValue b) =>
+        new ProduceValue(a.gold - b.gold, a.actions - b.actions);
+
+    public ProduceValue(int gold = 0, int actions = 0)
     {
         this.gold = gold;
         this.actions = actions;

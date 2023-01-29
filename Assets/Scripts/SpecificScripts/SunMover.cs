1c606eb682676c576e98b67dc15253b2309e9b13
Lolaperez <lolaperez1331@gmail.com>
2023-01-29 19:14:59 +0300

    SunMover updated

--- Assets/Scripts/SpecificScripts/SunMover.cs
+++ Assets/Scripts/SpecificScripts/SunMover.cs
@@ -1,9 +1,15 @@
-using UnityEngine;
-
 public class SunMover : SliderPerformer
 {
+    private TimesOfDay timeHandler;
+    private float lastX = -1;
+
+    private void Start()
+    {
+        timeHandler = GetComponent<TimesOfDay>();
+    }
     public override void Fulfill(float x)
     {
-        transform.rotation = Quaternion.Euler(x * 360, -30f, 0f);
+        if (lastX != x) timeHandler.SetTime(x);
+        lastX = x;
     }
 }

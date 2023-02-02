73ce644829f42b15b2b916a5a50f11eb36e1f4b4
Lolaperez <lolaperez1331@gmail.com>
2023-02-02 19:47:32 +0300

    Update Client.cs

--- Assets/Scripts/Client/Client.cs
+++ Assets/Scripts/Client/Client.cs
@@ -1,21 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
-using System.Net;
 using System.Text;
+using System;
+using System.Threading.Tasks;
 
 public class Client : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isConnected { get; protected set; } = false;
+    [SerializeField] private int port;
+    [SerializeField] private string host;
+
+    private NetworkStream stream;
+    private TcpClient client;
+    private Task<string> task;
+
+    public const int BUFFER_STANDART_SIZE = 1024;
+    public const int LARGE_BUFFER_SIZE = 2 * 65536;
+
+    public void Connect()
     {
-        
+        if (!isConnected)
+        {
+            // INITIALIZATION
+            client = new TcpClient(host, port);
+            stream = client.GetStream();
+
+            // ѕри соединении, сервер отправл€ет BoardStatement и Order => (PlayerInstance)
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (isConnected)
+        {
+            isConnected = client.Connected;
+            task = RecieveMessageAsync();
+            if (task.Status == TaskStatus.RanToCompletion)
+            {
+                // ѕолучен пакет
+
+                // ActionYandler.Handle(task.Result);
+
+                task = RecieveMessageAsync();
+            }
+        }
+    }
+
+
+    // Called from ActionHandler
+    public void SendPack(string pack)
+    {
+        SendMessageAsync(pack);
+    }
+
+    private async Task SendMessageAsync(string message, int buffSize = BUFFER_STANDART_SIZE)
+    {
+        await Task.Run(() => SendHandler(message, buffSize));
+    }
+
+    private void SendHandler(string message, int buffSize)
+    {
+        byte[] msg = new byte[buffSize];
+        msg = Encoding.Default.GetBytes(message);  // конвертируем строку в массив байт
+
+        stream.Write(msg, 0, msg.Length);     // отправл€ем сообщение
+    }
+
+
+    private async Task<String> RecieveMessageAsync(int buffSize = BUFFER_STANDART_SIZE)
+    {
+        return await Task<String>.Run(() => RecieveHandler(buffSize));
+    }
+
+
+    private string RecieveHandler(int buffSize)
+    {
+        byte[] msg = new byte[buffSize];     // готовим место дл€ прин€ти€ сообщени€
+        int count = stream.Read(msg, 0, msg.Length);   // читаем сообщение от клиента
+        return Encoding.Default.GetString(msg, 0, count); // выводим на экран полученное сообщение в виде строк
     }
 }
